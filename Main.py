ada3e56bd5ae76b98f3d94ac54437577ab298ef3
leonbornemann <leonbornemann@gmx.de>
2021-12-14 11:13:08 +0100

    Added parameters for Main.py

--- Main.py
+++ Main.py
@@ -7,80 +7,90 @@ import timeit
 from episode_mining.minepi import MINEPI, MinEpiRules
 from episode_mining.winepi import WINEPI, WinEpiRules
 
+class Main():
 
-def loadTimestampToEventDictFromFile(filePath):
-    file = open(filePath)
-    res = json.load(file)
-    file.close()
-    timestampToEventDict = {}
-    keysSorted = sorted(res.keys())
-    for i,key in enumerate(keysSorted):
-        timestamps = sorted(list(map(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d"), res[key])))
-        for ts in timestamps:
-            eventList = timestampToEventDict.get(ts)
-            if eventList is not None:
-                eventList.append(i)
+    def __init__(self,algorithmType,dataFile,width,step,minSupport,minConfidence,timestampStepsInHours):
+        self.algorithmType = algorithmType
+        self.dataFile = dataFile
+        self.width = int(width)
+        self.step = int(step)
+        self.minSupport = float(minSupport)
+        self.minConfidence = float(minConfidence)
+        self.timestampStepsInHours = int(timestampStepsInHours)
+
+    def loadTimestampToEventDictFromFile(self):
+        file = open(dataFile)
+        res = json.load(file)
+        file.close()
+        timestampToEventDict = {}
+        keysSorted = sorted(res.keys())
+        keysSorted = keysSorted[-50:]
+        for i,key in enumerate(keysSorted):
+            timestamps = sorted(list(map(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d"), res[key])))
+            for ts in timestamps:
+                eventList = timestampToEventDict.get(ts)
+                if eventList is not None:
+                    eventList.append(i)
+                else:
+                    newList = [i]
+                    timestampToEventDict[ts] = newList
+        return timestampToEventDict
+
+
+    def getTimestampToIntegerDict(self, timestampsSorted):
+        firstTimestamp = timestampsSorted[0]
+        lastTimestamp = timestampsSorted[-1]
+        # SOCRATA SPECIFIC: TODO - change this for Wikipedia!
+        curTimestamp = firstTimestamp
+        timestampToIntegerDict = {}
+        curInteger = 0
+        while curTimestamp != None:
+            timestampToIntegerDict[curTimestamp] = curInteger
+            curInteger += 1
+            if (curTimestamp == lastTimestamp):
+                curTimestamp = None
             else:
-                newList = [i]
-                timestampToEventDict[ts] = newList
-    return timestampToEventDict
+                curTimestamp = curTimestamp + datetime.timedelta(hours=self.timestampStepsInHours)
+        return timestampToIntegerDict
 
 
-def getTimestampToIntegerDict1DayIncrement(timestampsSorted):
-    firstTimestamp = timestampsSorted[0]
-    lastTimestamp = timestampsSorted[-1]
-    # SOCRATA SPECIFIC: TODO - change this for Wikipedia!
-    incrementInDays = 1
-    curTimestamp = firstTimestamp
-    timestampToIntegerDict = {}
-    curInteger = 0
-    while curTimestamp != None:
-        timestampToIntegerDict[curTimestamp] = curInteger
-        curInteger += 1
-        if (curTimestamp == lastTimestamp):
-            curTimestamp = None
+    def getFinalSequence(self,timestampsSorted, timestampToIntegerDict, timestampToEventDict):
+        finalList = []
+        for ts in timestampsSorted:
+            i = timestampToIntegerDict[ts]
+            for key in timestampToEventDict[ts]:
+                finalList.append((i, key))
+        return finalList
+
+
+    def main(self):
+        print("Starting Data Loading")
+        # print(timestampToEventDict)
+        timestampToEventDict = self.loadTimestampToEventDictFromFile()
+        timestampsSorted = sorted(timestampToEventDict.keys())
+        timestampToIntegerDict = self.getTimestampToIntegerDict(timestampsSorted)
+        sequence = self.getFinalSequence(timestampsSorted, timestampToIntegerDict, timestampToEventDict)
+        if (self.algorithmType == "WINEPI"):
+            print("Starting WINEPI")
+            sys.stdout.flush()
+            alg = WINEPI(sequence, episode_type='serial')
+            freqItems, suppData = alg.WinEpi(width=self.width, step=self.step, minFrequent=self.minSupport)
+            print("Done with WINEPI")
+            winepiRules = WinEpiRules(freqItems, suppData, width=self.width, minConfidence=self.minConfidence)
+            ruleList = winepiRules.generateRules()
+            winepiRules.printRules(ruleList)
+        elif (self.algorithmType == "MINEPI"):
+            print("Starting MINEPI")
+            sys.stdout.flush()
+            alg = MINEPI(sequence, episode_type='serial')
+            freqEpisodes = alg.MinEpi(max_width=self.width, step=self.step, minFrequent=self.minSupport)
+            print("Done with MINEPI")
+            minepiRules = MinEpiRules(freqEpisodes, max_width=self.width, step=self.step, minConfidence=self.minConfidence)
+            ruleList = minepiRules.generateRules()
+            minepiRules.printRules(ruleList)
         else:
-            curTimestamp = curTimestamp + datetime.timedelta(days=incrementInDays)
-    return timestampToIntegerDict
-
-
-def getFinalSequence(timestampsSorted, timestampToIntegerDict, timestampToEventDict):
-    finalList = []
-    for ts in timestampsSorted:
-        i = timestampToIntegerDict[ts]
-        for key in timestampToEventDict[ts]:
-            finalList.append((i, key))
-    return finalList
-
-
-def main(algorithmType, dataFile):
-    print("Starting Data Loading")
-    # print(timestampToEventDict)
-    timestampToEventDict = loadTimestampToEventDictFromFile(dataFile)
-    timestampsSorted = sorted(timestampToEventDict.keys())
-    timestampToIntegerDict = getTimestampToIntegerDict1DayIncrement(timestampsSorted)
-    sequence = getFinalSequence(timestampsSorted, timestampToIntegerDict, timestampToEventDict)
-    if (algorithmType == "WINEPI"):
-        print("Starting WINEPI")
-        sys.stdout.flush()
-        alg = WINEPI(sequence, episode_type='serial')
-        freqItems, suppData = alg.WinEpi(width=11, step=1, minFrequent=0.05)
-        print("Done with WINEPI")
-        winepiRules = WinEpiRules(freqItems, suppData, width=11, minConfidence=0.9)
-        ruleList = winepiRules.generateRules()
-        winepiRules.printRules(ruleList)
-    elif (algorithmType == "MINEPI"):
-        print("Starting MINEPI")
-        sys.stdout.flush()
-        alg = MINEPI(sequence, episode_type='serial')
-        freqEpisodes = alg.MinEpi(max_width=11, step=1, minFrequent=0.05)
-        print("Done with MINEPI")
-        minepiRules = MinEpiRules(freqEpisodes, max_width=11, step=1, minConfidence=0.9)
-        ruleList = minepiRules.generateRules()
-        minepiRules.printRules(ruleList)
-    else:
-        print("Unknown algorithm type")
-    print("Done with Everything")
+            print("Unknown algorithm type")
+        print("Done with Everything")
 
 
 if __name__ == "__main__":
@@ -90,8 +100,17 @@ if __name__ == "__main__":
     sys.stdout.flush()
     algorithmType = sys.argv[1]
     dataFile = sys.argv[2]
+    width = sys.argv[3]
+    step = sys.argv[4]
+    minSupport = sys.argv[5]
+    minConfidence = sys.argv[6]
+    timestampStepsInHours = sys.argv[7]
+    #width=11, step=1, minFrequent=0.05)
+        #print("Done with WINEPI")
+        #winepiRules = WinEpiRules(freqItems, suppData, width=11, minConfidence=0.9)
     start = timeit.default_timer()
-    main(algorithmType, dataFile)
+    mainObject = Main(algorithmType,dataFile,width,step,minSupport,minConfidence,timestampStepsInHours)
+    mainObject.main()
     stop = timeit.default_timer()
     print('Total Time: ', stop - start)
     sys.stdout.flush()
